71a104de423b70c7a2839cd465d6d5028d378e7b
shiwei.yang <shiwei.yang@horizon.ai>
2022-11-15 17:18:30 +0800

    +update: use metadata to process the incoming context

--- generator/generate.go
+++ generator/generate.go
@@ -32,7 +32,8 @@ func GenerateFile(gen *protogen.Plugin, file *protogen.File) *protogen.Generated
 	g.P()
 	g.P("// This is a compile-time assertion to ensure that this generated file")
 	g.P("// is compatible with the github.com/x-lambda/protoc-gen-gin package it is being compiled against.")
-	g.P("// ", contextPkg.Ident("")) // TODO metadata
+	g.P("// ", contextPkg.Ident(""))
+	g.P("// ", metadataPkg.Ident(""))
 	g.P("// ", ginPkg.Ident(""), errPkg.Ident(""))
 	g.P()
 
